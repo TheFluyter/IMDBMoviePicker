9d872357e600bf054c5819f8d6dd1a12b2e44bb5
TheFluyter <rick__ruiter@hotmail.com>
2021-01-27 20:38:39 +0100

    new getDifferentMovie method

--- src/GUI.java
+++ src/GUI.java
@@ -7,30 +7,66 @@ public class GUI implements ActionListener {
 
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
-    JLabel labelInstructions = new JLabel("Upload your IDMB CSV movie list!", JLabel.CENTER);
-    JButton uploadButton = new JButton("Upload");
-    JLabel labelMovie = new JLabel("", JLabel.CENTER);
+    JButton uploadButton, tryAgainButton;
+    JLabel movieLabel, instructionsLabel;
 
     public GUI() {
         panel.setBorder(BorderFactory.createEmptyBorder(50, 150, 100, 150));
         panel.setLayout(new GridLayout(0, 1));
-        panel.add(labelInstructions);
-        panel.add(uploadButton);
-        panel.add(labelMovie);
 
-        labelInstructions.setPreferredSize(new Dimension(300, 30));
+        setLabels();
+        setButtons();
+        setActions();
+        setFrame();
+    }
+
+    private void setLabels() {
+        instructionsLabel = new JLabel("Upload your IMDB CSV movie list!", JLabel.CENTER);
+        instructionsLabel.setPreferredSize(new Dimension(300, 30));
+        panel.add(instructionsLabel);
+
+        movieLabel = new JLabel("", JLabel.CENTER);
+        panel.add(movieLabel);
+    }
+
+    private void setButtons() {
+        uploadButton = new JButton("Upload");
         uploadButton.setPreferredSize(new Dimension(100, 30));
-        uploadButton.addActionListener(this);
+        panel.add(uploadButton);
 
+        tryAgainButton = new JButton("Try Again!");
+        tryAgainButton.setPreferredSize(new Dimension(100, 30));
+        panel.add(tryAgainButton);
+        tryAgainButton.setVisible(false);
+    }
+
+    private void setActions() {
+        uploadButton.addActionListener(this);
+        uploadButton.setActionCommand("uploadButton");
+
+        tryAgainButton.addActionListener(this);
+        tryAgainButton.setActionCommand("tryAgainButton");
+    }
+
+    private void setFrame() {
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("IMDB Movie Picker");
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        labelMovie.setText(new MoviePicker().getMovie());
+
+        String command = e.getActionCommand();
+
+        if (command.equals("uploadButton")) {
+            movieLabel.setText("Let's watch: '" + MoviePicker.getMovie() + "'");
+            tryAgainButton.setVisible(true);
+        } else if (command.equals("tryAgainButton")) {
+            movieLabel.setText("Let's watch: '" + MoviePicker.getDifferentMovie() + "'");
+        }
     }
 }
--- src/MoviePicker.java
+++ src/MoviePicker.java
@@ -6,20 +6,33 @@ public class MoviePicker {
     private static List<String> movies;
     private static String movie;
 
-    public static void main(String[] args) { launch(); }
+    public static void main(String[] args) { new GUI(); }
 
-    private static void launch() {
-        new GUI();
-    }
-
-    private static String getRandomMovie(List<String> movies) {
-        int random = new Random().nextInt(movies.size());
-        return movies.get(random);
-    }
-
-    public String getMovie() {
+    // Get IMDB movie list through CSV reader and pick a random movie
+    public static String getMovie() {
         movies = new CSVReader().getMovieList();
-        movie = getRandomMovie(movies);
+        int random = new Random().nextInt(movies.size());
+        movie = movies.get(random);
         return movie;
+    }
+
+    // Pick a different movie, but don't change the movie list. Available after getMovie method.
+    public static String getDifferentMovie() {
+        int random = new Random().nextInt(movies.size());
+        String newMovie = movies.get(random);
+
+        if (!newMovie.equals(movie)) {
+            movie = newMovie;
+            return newMovie;
+
+        } else if (random + 1 < movies.size()) {
+            movie = movies.get(random + 1);
+            return movie;
+
+        } else if (random - 1 != 0) {
+            movie = movies.get(random - 1);
+            return movie;
+        }
+        return newMovie;
     }
 }
